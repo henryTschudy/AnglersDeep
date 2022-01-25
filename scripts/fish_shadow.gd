0aec7678c02917a3bc0e39ed9d90367e70023f5b
GitHusbando <67436617+GitHusbando@users.noreply.github.com>
2022-01-25 01:08:14 -0800

    added preliminary fish shadow spawning
    spawns fish by generating a possible fish coordinate in a box around the player, then checking if it's a valid spawn area. if it is, spawns a fish, otherwise tries again next loop. works okay, but could probably be better. culls far-away fish and keeps 10 at a time

---
+++ scripts/fish_shadow.gd
@@ -0,0 +1,67 @@
+extends KinematicBody2D
+
+var COLLISION_RADIUS = 10
+
+var fish_type = "placeholder" #might want to change this to like an index or something, could use enums even idk, use 
+var fish_direction
+var fish_speed
+
+var fish_collision_shape
+var fish_sprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready(): #may need to do some freeing
+	random_speed_and_direction()
+	
+	fish_collision_shape = CollisionShape2D.new()
+	fish_collision_shape.shape = CircleShape2D.new()
+	fish_collision_shape.shape.set_radius(COLLISION_RADIUS)
+	
+	fish_sprite = Sprite.new()
+	fish_sprite.set_texture(load("res://textures/fish_shadows/shadowfish.png"))
+	
+	set_collision_layer_bit(0, false)
+	set_collision_layer_bit(1, true)
+	set_collision_mask_bit(0, false)
+	set_collision_mask_bit(1, true)
+	
+	add_child(fish_collision_shape)
+	add_child(fish_sprite)
+
+func _physics_process(delta):
+	swim()
+
+func set_fish_type(given_string): #this function could call another function that gets info from a json and sets it
+	fish_type = given_string
+
+func get_fish_type():
+	return fish_type
+
+func swim():
+	move_and_slide(fish_speed*fish_direction)
+
+func random_speed_and_direction():
+	change_direction_random()
+	change_speed_random()
+
+func change_direction_random():
+	fish_direction = Vector2(rand_range(-1, 1),rand_range(-1,1)).normalized()
+	
+func change_direction(given_direction): #please give a Vector2
+	fish_direction = given_direction
+
+func change_speed_random():
+	fish_speed = rand_range(0, 5)
+	
+func change_speed(given_speed):
+	fish_speed = given_speed
+
+func is_in_distance(center, x_distance, y_distance):
+	return position.x > center.x - x_distance && position.x < center.x + x_distance && position.y > center.y - y_distance && position.y < center.y + y_distance
+
+func despawn():
+	queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ scripts/fish_spawn.gd
@@ -0,0 +1,66 @@
+extends Node
+
+onready var tilemap = get_node("TileMap")
+onready var boat = get_node("../boat")
+var fish_shadows = []
+
+var SPAWN_DISTANCE_X = 480 #there is probably a better way to get this distance from the camera
+var SPAWN_DISTANCE_Y = 270
+var SPAWN_AREA_OUTER = 1000
+var MAX_FISH = 10
+
+var ShadowFishy = load("res://scripts/fish_shadow.gd")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func spawn_fish(coordinates):
+	var new_fish = ShadowFishy.new()
+	new_fish.set_fish_type("shadowfish")
+	new_fish.position = coordinates
+	
+	owner.add_child(new_fish)
+	fish_shadows.push_back(new_fish)
+	
+func try_generate_spawn_fish():
+	var try_coordinates = generate_spawn_coordinates()
+	var map_rect = tilemap.get_used_rect()
+	
+	if (map_rect.has_point(try_coordinates)):
+		print("1")
+		return
+	if (tilemap.get_cellv(tilemap.world_to_map(try_coordinates)) != $TileMap.INVALID_CELL):
+		print("2")
+		return
+	
+	spawn_fish(try_coordinates)
+
+func generate_spawn_coordinates(): #generate some coordinates just outside the field of view
+	 # i THINK 0 is empty??
+	
+	var distance_x = rand_range(-SPAWN_AREA_OUTER, SPAWN_AREA_OUTER)
+	var distance_y = rand_range(-SPAWN_AREA_OUTER, SPAWN_AREA_OUTER)
+	
+	if (distance_x < 0):
+		distance_x -= SPAWN_DISTANCE_X
+	else:
+		distance_x += SPAWN_DISTANCE_X
+	
+	if (distance_y < 0):
+		distance_y -= SPAWN_DISTANCE_Y
+	else:
+		distance_y += SPAWN_DISTANCE_Y
+	
+	return Vector2(distance_x, distance_y) + boat.position
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if fish_shadows.size() < MAX_FISH :
+		try_generate_spawn_fish()
+	
+	for shadow_fish in fish_shadows:
+		if (!shadow_fish.is_in_distance(boat.position, SPAWN_DISTANCE_X + SPAWN_AREA_OUTER/2, SPAWN_DISTANCE_Y + SPAWN_AREA_OUTER/2)):
+			fish_shadows.erase(shadow_fish) 
+			shadow_fish.despawn()
