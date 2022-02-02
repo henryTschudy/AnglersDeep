936d478ba115221bea91954717c1f3e71ccb7af3
GitHusbando <67436617+GitHusbando@users.noreply.github.com>
2022-02-02 15:49:23 -0800

    reeling game fixed in overworld, made fish movement a bit nicer
    I also changed winning and losing to be based on distance rather than screen edges since I've been getting the impression people would prefer that and added a distance meter so it's readable.  I think win conditions are working properly,  but it needs more testing. Still haven't figured out the slingshot problem.

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -1,17 +1,24 @@
 extends KinematicBody2D
 
 var FISH_ANGER_INCREMENT = 10
+var FISH_TURN_SLOWNESS = 5
+var FISH_AWAY_TENDENCY = 0.5 #tendency for the fish to swim away from the line, keep this number pretty low
+var WIN_DISTANCE = 150
+var LOSS_DISTANCE = 1000
 
 onready var fish_state_timer = get_node("fish_state_timer")
 onready var fish_swim_timer = get_node("fish_swim_timer")
+onready var line = get_node("../line")
 
 var screen_size = Vector2(0,0)
 
+var hook_offset_base_y = 0
 var hook_offset = Vector2(0,0)
 var reel_state = false
 
 var fish_type
 
+var fish_direction_target
 var fish_direction
 var fish_speed
 var fish_anger
@@ -21,13 +28,15 @@ var fish_caught = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size.x = get_viewport().get_visible_rect().size.x # Get width
-	screen_size.y = get_viewport().get_visible_rect().size.y # Get height
+	screen_size.x = get_viewport().get_visible_rect().size.x #* get_parent().scale # Get width
+	screen_size.y = get_viewport().get_visible_rect().size.y #* get_parent().scale # Get height
 	
 	hook_offset = Vector2(100, 35)
+	hook_offset_base_y = hook_offset.y
 	fish_state_timer.connect("timeout",self,"on_fish_state_timer_timeout")  
 	fish_swim_timer.connect("timeout",self,"on_fish_swim_timer_timeout") 
 	fish_direction = Vector2(0,0)
+	fish_direction_target = Vector2(0,0)
 	fish_speed = 1
 	fish_anger = 0
 	
@@ -44,8 +53,9 @@ func _physics_process(_delta): #incorporate delta, dumpass
 		fish_swim()
 
 func fish_swim():
+	fish_direction = (fish_direction + fish_direction_target/FISH_TURN_SLOWNESS).normalized()
 	move_fish(fish_speed, fish_direction)
-	check_offscreen()
+	check_win_loss_condition()
 	
 func fish_get_dragged():
 	move_fish(fish_speed, fish_direction)
@@ -54,8 +64,13 @@ func fish_get_dragged():
 func move_fish(move_speed, move_direction):
 	#print(move_speed)
 	#print(move_direction)
+	if fish_direction.x < 0:
+		set_fish_flipped(true)
+	else:
+		set_fish_flipped(false)
+	
 	rotation = move_direction.angle()
-	move_and_slide(move_speed*move_direction)
+	move_and_slide(get_parent().scale.x * move_speed*move_direction)
 
 func on_fish_state_timer_timeout():
 	reel_state = !reel_state
@@ -68,17 +83,20 @@ func on_fish_state_timer_timeout():
 	
 func on_fish_swim_timer_timeout():
 	#print(fish_swim_timer.paused)
-	fish_direction = Vector2( rand_range(0,2)-1, rand_range(0,2)-rand_range(1,3) ).normalized() #tendency to go upward
+	#fish_direction = Vector2( rand_range(0,2)-1, rand_range(0,2)-rand_range(1,3) ).normalized() #tendency to go upward
+	var fish_direction_target_base = ( Vector2(rand_range(-1,1), rand_range(-1,1)) ).normalized()
+	var fish_direction_target_addon = FISH_AWAY_TENDENCY * ( position - line.points[0] ).normalized()
+	fish_direction_target = (fish_direction_target_base + fish_direction_target_addon).normalized()
 	fish_speed = rand_range(50,200)
 	
-func check_offscreen(): #will need to redo this to be more flexible
+func check_win_loss_condition(): #will need to redo this to be more flexible
 	#if (position + hook_offset).x >= screen_size.x || (position + hook_offset).x <= 0:
 	#	fish_direction.x = -fish_direction.x
 	#if (position + hook_offset).y >= screen_size.y || (position + hook_offset).y <= 0:
 	#	fish_direction.y = -fish_direction.y
-	if (position + hook_offset).x >= screen_size.x || (position + hook_offset).x <= 0 || (position + hook_offset).y <= 0:
+	if get_fish_distance() > (LOSS_DISTANCE * get_parent().scale.x):
 		fish_escaped = true
-	elif (position + hook_offset).y >= screen_size.y: #for now, winning just checks if fish goes offscreen downward
+	elif get_fish_distance() < (WIN_DISTANCE * get_parent().scale.x): #for now, winning just checks fish distance from line base
 		fish_caught = true
 
 func set_fish_type(given_fish_type):
@@ -86,3 +104,13 @@ func set_fish_type(given_fish_type):
 	
 func get_fish_type():
 	return fish_type
+	
+func get_fish_distance():
+	return (position + hook_offset).distance_to(line.points[0]) * get_parent().scale.x
+
+func set_fish_flipped(flip_state):
+	get_node("fish_sprite").set_flip_v(flip_state)
+	if (flip_state):
+		hook_offset.y = -1*hook_offset_base_y
+	else:
+		hook_offset.y = hook_offset_base_y
--- scripts/reel_scene_manager.gd
+++ scripts/reel_scene_manager.gd
@@ -5,11 +5,12 @@ signal reel_game_over(game_lost, fish_type)
 var DRAG_SPEED = 65
 var DRAG_SPEED_SWIM = 45
 
-var screen_size = Vector2(0,0)
+#var screen_size = Vector2(0,0)
 
 onready var fishy = get_node("fish")
 onready var line = get_node("line")
 onready var fish_anger_bar = get_node("fish_anger_bar")
+onready var fish_distance_bar = get_node("fish_distance_bar")
 
 #to make fish swim in the direction it was swimming before reeling in during swim mode
 var fish_prev_direction = Vector2(0,0)
@@ -19,12 +20,15 @@ var fishy_angry = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size.x = get_viewport().get_visible_rect().size.x # Get width
-	screen_size.y = get_viewport().get_visible_rect().size.y # Get height
+	#screen_size.x = get_viewport().get_visible_rect().size.x * get_parent().scale.x # Get width
+	#screen_size.y = get_viewport().get_visible_rect().size.y * get_parent().scale.y # Get height
+	fish_distance_bar.min_value = fishy.WIN_DISTANCE
+	fish_distance_bar.max_value = fishy.LOSS_DISTANCE
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	line.points[1] = fishy.global_position + fishy.hook_offset.rotated(fishy.fish_direction.angle()) - line.global_position
+	line.points[1] = fishy.position + fishy.hook_offset.rotated(fishy.fish_direction.angle()) - line.position
+	fish_distance_bar.set_value(fishy.get_fish_distance())
 	if fishy_angry:
 		fishy.fish_anger += fishy.FISH_ANGER_INCREMENT * delta
 		fish_anger_bar.set_value(fishy.fish_anger)
@@ -37,14 +41,14 @@ func _process(delta):
 func _unhandled_input(_event):
 	if Input.is_action_pressed("reel_in"):
 		if fishy.reel_state:
-			fishy.fish_direction = (fishy.global_position.direction_to(line.points[0] + line.global_position)).normalized()
+			fishy.fish_direction = (fishy.position.direction_to(line.points[0] + line.position)).normalized()
 			fishy.fish_speed = DRAG_SPEED
 			#print(line.points[0] + line.global_position)
 			#print("reeling in")
 		else:
 			fish_prev_direction = fishy.fish_direction
 			fish_prev_speed = fishy.fish_speed
-			fishy.fish_direction = (fishy.global_position.direction_to(line.points[0] + line.global_position)).normalized()
+			fishy.fish_direction = (fishy.position.direction_to(line.points[0] + line.position)).normalized()
 			fishy.fish_speed = DRAG_SPEED_SWIM
 			fishy_angry = true
 			
