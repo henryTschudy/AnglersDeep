f330c7c9d5b3aaec81e8d3b7b604b4bfda848927
William Propp <33852102+williamPropp@users.noreply.github.com>
2022-03-09 20:37:07 -0800

    updated sound sliders to use a logarithmic scale instead of linear scale

--- scripts/settings_menu.gd
+++ scripts/settings_menu.gd
@@ -6,15 +6,15 @@ func _ready():
 	var sound_fx_volume_slider = get_node("sound_fx")
 	
 	#load existing volume values into sliders, need to add feature to load values from save data
-	master_volume_slider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
-	music_volume_slider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music"))
-	sound_fx_volume_slider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Sound_fx"))
+	master_volume_slider.value = db2linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
+	music_volume_slider.value = db2linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music")))
+	sound_fx_volume_slider.value = db2linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Sound_fx")))
 	
 func _on_master_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear2db(value))
 	
 func _on_music_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear2db(value))
 
 func _on_sound_fx_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SoundFx"), value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SoundFx"), linear2db(value))
